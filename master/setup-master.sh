a502c29d230c756310089fcba8bd5803d13e0d63
Gareth Brown <gaz@gbmail.me>
2020-09-25 18:56:33 +0100

    start merging master/slave into single image

--- master/setup-master.sh
+++ master/setup-master.sh
@@ -1,20 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-PG_REP_PASSWORD=$(cat $PG_REP_PASSWORD_FILE)
+if ! ${PG_MASTER:-false}; then
+  echo "\$PG_MASTER not set. Exiting master DB setup"
+  exit 0
+fi
+
+PG_REP_PASSWORD=$(cat "${PG_REP_PASSWORD_FILE}")
+
+set -e
+source /usr/local/bin/docker-entrypoint.sh
+
+docker_process_sql <<<"
+  DO \$$
+  BEGIN
+    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname='$PG_REP_USER') THEN
+      CREATE ROLE $PG_REP_USER WITH REPLICATION PASSWORD '$PG_REP_PASSWORD' LOGIN;
+    END IF;
+  END
+  \$$
+"
+
+#"SELECT 1 FROM pg_roles WHERE rolname='USR_NAME'" | grep -q 1 ||
 
 echo "host replication all ${HBA_ADDRESS} md5" >> "$PGDATA/pg_hba.conf"
 
-set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER $PG_REP_USER REPLICATION LOGIN CONNECTION LIMIT 100 ENCRYPTED PASSWORD '$PG_REP_PASSWORD';
-EOSQL
-
-cat >> ${PGDATA}/postgresql.conf <<EOF
-wal_level = hot_standby
-archive_mode = on
-archive_command = 'cd .'
-max_wal_senders = 8
-wal_keep_segments = 8
-hot_standby = on
-synchronous_standby_names = '*'
-EOF
+# replication specific configuration
+{
+  echo "wal_level = hot_standby"
+  echo "archive_mode = on"
+  echo "archive_command = 'cd .'"
+  echo "max_wal_senders = 5"
+  echo "wal_keep_segments = 32"
+  echo "hot_standby = on"
+  echo "synchronous_standby_names = '*'"
+} >> $PGDATA/postgresql.conf
