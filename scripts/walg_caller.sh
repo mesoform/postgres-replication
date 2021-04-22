32a70db7e8af756bacec22fa88c5373d368693c9
danperezsan <dan@mesoform.com>
2021-04-22 18:36:59 +0200

    - Add PGPORT variable to handle ports other than postgres default 5432

--- scripts/walg_caller.sh
+++ scripts/walg_caller.sh
@@ -5,11 +5,12 @@ export WALG_GS_PREFIX=STORAGEBUCKET/CONTAINERDATE
 export PGUSER=POSTGRESUSER
 export PGDATABASE=POSTGRESDB
 export PGHOST=/var/run/postgresql
-export PGPORT=5432
+export PGPORT=$PGPORT
 echo "GOOGLE_APPLICATION_CREDENTIALS: $GOOGLE_APPLICATION_CREDENTIALS"
 echo "WALG_GS_PREFIX: $WALG_GS_PREFIX"
 echo "PGUSER: $PGUSER"
 echo "PGDATABASE: $PGDATABASE"
+echo "PGPORT: $PGPORT"
 
 echo "Running command /usr/local/bin/wal-g $1 $2 $3"
 /usr/local/bin/wal-g $1 $2 $3
